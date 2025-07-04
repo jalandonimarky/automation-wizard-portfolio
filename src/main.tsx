69157a8ee81a0a3f2fef0065e13efc57828187ba
jalandonimarky <markyjalandoni.mj@gmail.com>
2025-07-04 18:34:51 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/automation-wizard-portfolio">
-    <App />
-  </BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/automation-wizard-portfolio">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
