b9de7e1c280406176e6143a3a5734d62838fc7fc
jalandonimarky <markyjalandoni.mj@gmail.com>
2025-07-04 20:43:13 +0800

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,6 @@ const Hero = () => {
             <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-purple-400 via-pink-400 to-blue-400 bg-clip-text text-transparent">
               Hi, I am Marky!
             </h1>
-            <br> </br>
             <h2 className="text-2xl md:text-3xl text-gray-300 mb-8 font-light">
               Technical Engineer/Automation Specialist & API Developer
             </h2>
